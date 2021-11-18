153901aa728d820830a5bffc934420e086770132
Cankar001 <info.can-karka@web.de>
2021-11-18 17:39:09 +0100

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "GoogleTest"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"googletest/include/**.h",
+		"googletest/src/**.cc",
+		"googlemock/include/**.h",
+		"googlemock/src/**.cc",
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+	
